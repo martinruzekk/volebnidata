c63a5a6398d27c9a8e7801f834ff0e9572376d70
Martin Růžek <mata.ruzek@gmail.com>
2020-12-18 22:17:32 +0100

    Simple router, simple templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends AbstractController
 {
@@ -12,8 +14,19 @@ class MainController extends AbstractController
      */
     public function index()
     {
-        return $this->render('main.html.twig', [
-            'controller_name' => 'MainController',
+        return $this->render('main.html.twig');
+    }
+
+    /**
+     * @Route("/app/{volby?}", name="app")
+     */
+    public function app(Request $request, $volby)
+    {
+        /*if ($volby == "ps2017") {
+            return new Response("ahoj");
+        }*/
+        return $this->render('app.html.twig', [
+            'volby' => $volby
         ]);
     }
 }
